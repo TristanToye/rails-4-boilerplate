51c9d46bdf2e08ba86d706456f3a004c4faa1a52
Tristan <tristantoye@gmail.com>
2015-01-27 11:21:40 -0500

    More typos.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,9 +6,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 				if @user.persisted?
 					sign_in_and_redirect @user, event: :authentication
-					set_flash)message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+					set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
 				else
-					sesson["devise.#{provider}_data"] = env["omniauth.auth]
+					sesson["devise.#{provider}_data"] = env["omniauth.auth"]
 					redirect_to new_user_registration_url
 				end
 			end
