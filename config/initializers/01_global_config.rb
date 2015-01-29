57f71c33c2a4fa4bd09f96697ae801ea1ac770d2
Tristan <tristantoye@gmail.com>
2015-01-29 15:42:41 -0500

    Checking if table present in initializer.

--- config/initializers/01_global_config.rb
+++ config/initializers/01_global_config.rb
@@ -1,6 +1,8 @@
 #Check we have generated a gonfig record
-if GlobalConfig.all.count != 0
-	GlobalConfig.first.update_global_config
-else
-	GlobalConfig.create
+if GlobalConfig.table_exists?
+	if GlobalConfig.all.count != 0
+		GlobalConfig.first.update_global_config
+	else
+		GlobalConfig.create
+	end
 end
--- lib/tasks/assets_clean_enhance.rake
+++
@@ -1,3 +0,0 @@
-Rake::Task['assets:clean'].enhance do
-  Rake::Task['db:migrate'].invoke
-end
